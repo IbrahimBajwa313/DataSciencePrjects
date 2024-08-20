6e4949b93f21c16355bc3f0446234a871615a6b8
Ibrahim Bajwa <129992649+IbrahimBajwa313@users.noreply.github.com>
2024-08-20 12:52:34 +0500

    bug removed

--- Kmean_clustering.py
+++ Kmean_clustering.py
@@ -4,5 +4,28 @@ import seaborn as sns
 
 ds = pd.read_csv("kmeans_dataset.csv")
 
-sns.pairplot(ds)
+# sns.pairplot(ds)
+# plt.show()
+
+from sklearn.cluster import KMeans
+
+wcss = []
+
+for i in range( 2,21 ):
+    km = KMeans( n_clusters = i , init= 'k-means++')
+    km.fit(ds)
+    wcss.append(km.inertia_)
+
+
+
+plt.plot([i for i in range(2,21)],wcss,marker='o')
+plt.xlabel("No. of Cluster")
+plt.ylabel("WCSS")
+plt.xticks([i for i in range(2,21)])
+plt.grid(axis='x' )
 plt.show()
+
+knn = KMeans(n_clusters = '3')
+ds['Predict']= knn.fit_predict(ds)
+
+print(ds)
