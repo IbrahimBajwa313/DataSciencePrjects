044a9e9460822c6a8ea3fda27ae2a5d682692be7
Ibrahim Bajwa <129992649+IbrahimBajwa313@users.noreply.github.com>
2024-08-20 12:57:33 +0500

    Bug resolved

--- Kmean_clustering.py
+++ Kmean_clustering.py
@@ -1,31 +1,30 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.cluster import KMeans
 
 ds = pd.read_csv("kmeans_dataset.csv")
 
-# sns.pairplot(ds)
-# plt.show()
-
-from sklearn.cluster import KMeans
-
+# Calculate WCSS (Within-Cluster Sum of Squares) for different cluster counts
 wcss = []
-
-for i in range( 2,21 ):
-    km = KMeans( n_clusters = i , init= 'k-means++')
+for i in range(2, 21):
+    km = KMeans(n_clusters=i, init='k-means++')
     km.fit(ds)
     wcss.append(km.inertia_)
 
-
-
-plt.plot([i for i in range(2,21)],wcss,marker='o')
-plt.xlabel("No. of Cluster")
+# Plot the Elbow method graph
+plt.plot([i for i in range(2, 21)], wcss, marker='o')
+plt.xlabel("No. of Clusters")
 plt.ylabel("WCSS")
-plt.xticks([i for i in range(2,21)])
-plt.grid(axis='x' )
+plt.xticks([i for i in range(2, 21)])
+plt.grid(axis='x')
 plt.show()
 
-knn = KMeans(n_clusters = '3')
-ds['Predict']= knn.fit_predict(ds)
+# Apply KMeans with the chosen number of clusters
+knn = KMeans(n_clusters=3)  # Corrected: '3' -> 3
+ds['Predict'] = knn.fit_predict(ds)
 
-print(ds)
+# print(ds)
+
+sns.pairplot(ds, hue = 'Predict')
+plt.show()
